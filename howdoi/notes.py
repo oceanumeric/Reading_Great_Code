cff5fc8920f50baab0f66aa5de4df19a88af8dad
oceanumeric <numerical.ocean@gmail.com>
2021-09-22 15:42:00 +0200

    [update] notes.py

--- howdoi/notes.py
+++ howdoi/notes.py
@@ -1,80 +1,103 @@
 import argparse
+import textwrap
 import sys
 
-# resources
-# https://mkaz.blog/code/python-argparse-cookbook/
 
-## Example 1
+class IntRange:
+    def __init__(self, imin=None, imax=None) -> None:
+        self.imin = imin
+        self.imax = imax
+        
+    def __call__(self, arg):
+        try:
+            value = int(arg)
+        except ValueError as value_error:
+            raise self.exception() from value_error
+        if (self.imin is not None 
+            and value < self.imin) or (self.imax is not None 
+                                       and value > self.imax):
+            raise self.exception()
+        return value
+            
+    def exception(self):
+        if self.imin is not None and self.imax is not None:
+            return argparse.ArgumentTypeError(
+                f'Must be an integer in the range [{self.imin}, {self.imax}]'
+                )
+        if self.imin is not None:
+            return argparse.ArgumentTypeError(f'Must be an integer >= {self.imin}')
+        if self.imax is not None:
+            return argparse.ArgumentTypeError(f'Must be an integer <= {self.imax}')
+        return argparse.ArgumentTypeError('Must be an integer')
+            
 
-# parser = argparse.ArgumentParser(description='Process some integers.')
-# parser.add_argument('integers', metavar='N', type=int, nargs='+',
-#                     help='an integer for the accumulator')
-# parser.add_argument('--sum', dest='accumulate', action='store_const',
-#                     const=sum, default=max,
-#                     help='sum the integers (default: find the max)')
+def get_parser():
+    parser = argparse.ArgumentParser(
+        description='instant coding answers via the command line',
+        epilog=textwrap.dedent('''\
+            environment variable examples:
+                HOWDOI_COLORIZE=1
+                HOWDOI_DISABLE_CACHE=1
+                HOWDOI_DISABLE_SSL=1
+                HOWDOI_SEARCH_ENGINE=google
+                HOWDOI_URL=serverfault.com
+            '''),
+            formatter_class=argparse.RawTextHelpFormatter
+    )
+    parser.add_argument('query', metavar='QUERY', type=str, nargs='*', 
+                        help='the question to answer')
+    parser.add_argument('-p', '--pos', 
+                        help='select answer in specified postion (default: 1)',
+                        default=1, type=IntRange(1, 20), metavar='POS')
+    parser.add_argument('-n', '--num', 
+                        help='number of answers to return (default: 1)',
+                        dest='num_answers', default=1, type=IntRange(1, 20), 
+                        metavar='NUM')
+    parser.add_argument('--num-answers', help=argparse.SUPPRESS)
+    parser.add_argument('-a', '--all', 
+                        help='display the full text of the answer',
+                        action='store_true')
+    parser.add_argument('-l', '--link', help='display only the answer link',
+                        action='store_true')
+    parser.add_argument('-c', '--color', help='enable colorized output',
+                        action='store_true')
+    parser.add_argument('-x', '--explain', help='explain how answer was chosen',
+                        action='store_true')
+    parser.add_argument('-C', '--clear-cache', help='clear the cache',
+                        action='store_true')
+    parser.add_argument('-j', '--json', help='return answers in raw json format',
+                        dest='json_output',
+                        action='store_true')
+    parser.add_argument('--json-output', action='store_true',
+                        help=argparse.SUPPRESS)
+    parser.add_argument('-v', '--version',
+                        help='display the current version of howdoi',
+                        action='store_true')
+    parser.add_argument('-e', '--engine',
+                        help='search engine for this query \
+                            (google, bing, duckduckgo)',
+                        dest='search_engine', nargs="?",
+                        metavar='ENGINE')
+    parser.add_argument('--save', '--stash', help='stash a howdoi answer',
+                        action='store_true')
+    parser.add_argument('--view', help='view your stash',
+                        action='store_true')
+    parser.add_argument('--remove', help='remove an entry in your stash',
+                        action='store_true')
+    parser.add_argument('--empty', help='empty your stash',
+                        action='store_true')
+    parser.add_argument('--sanity-check', help=argparse.SUPPRESS,
+                        action='store_true')
+    return parser
 
-# args = parser.parse_args()
-# print(args.accumulate(args.integers))
 
-## Example 2
-
-# if len(sys.argv) > 3:
-#     print('You have specified too many arguments')
-#     sys.exit()
-
-# if len(sys.argv) < 2:
-#     print('You need to specify the path to be listed')
-#     sys.exit()
-
-# input_value0, input_value1 = sys.argv[0], sys.argv[1]
-
-# print(input_value0, input_value1)
-
-## Example 3
-
-# create a parser
-parser = argparse.ArgumentParser(
-    description='Demostration of parse in Python'
-)
-# add an argument
-parser.add_argument(
-    'namefp',   # it will become positional argument without --
-    metavar="fp", 
-    type=str,
-    help='Print the first input'
-)
-
-parser.add_argument(
-    '--verbose',
-    action='store_true',
-    help='Conditional print'
-)
-
-parser.add_argument(
-    '-list',
-    nargs=2,
-    type=int # it will be a list of str if no type was specified
-)
-
-parser.add_argument(
-    '-str',
-    nargs='*',  # it will accept all arguments 
-)
-
-parser.add_argument(
-    '--sum',
-    nargs='*',
-    type=int,
-    help="Return the sum of all intergers after it"
-)
-
-# Execute the parse_args() method
-args = parser.parse_args()
-input_value0 = args.namefp
-
-if args.verbose:  # args.ingore -- 
-    print('haha, very verbose')
-else:
-    print('no verbose argument passed')
-
-print(input_value0, args.list, args.str, sum(args.sum))
+if __name__ == '__main__':
+    print(sys.argv[0])
+    try: 
+        if sys.argv[1] == 'a':
+            print('Lovely, you made it')
+    except:
+        None
+    parser = get_parser()
+    args = vars(parser.parse_args())
+    print(args)
